0170153015ed2dab6fcd817ff77ca082fb299f6f
sivashiv23 <sivasankari1293@gmail.com>
2019-02-10 10:25:32 +0530

    Flight status added

--- src/main/java/com/stepdefinition/TestRunner.java
+++ src/main/java/com/stepdefinition/TestRunner.java
@@ -1,6 +1,12 @@
 package com.stepdefinition;
 
+import java.io.File;
+
+import org.joda.time.LocalDateTime;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+
+import com.resources.ExtentCucumberFormatter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -8,8 +14,24 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features = "src/test/java/feature/FlightStatus.feature", glue = { "com.stepdefinition" }, plugin = {
-		"html:target" })
+		"json:src/main/java/AutomationSuite/jsonReport.json", "html:target" })
 
 public class TestRunner {
 
+	@BeforeClass
+	public static void beforeClass() {
+		LocalDateTime now = LocalDateTime.now();
+		int year = now.getYear();
+		int month = now.getMonthOfYear();
+		int day = now.getDayOfMonth();
+		int hour = now.getHourOfDay();
+		int minute = now.getMinuteOfHour();
+		int second = now.getSecondOfMinute();
+		String snewFilename1 = year + "_" + month + "_" + day + "_" + hour + "_" + minute + "_" + second;
+		String filePath = System.getProperty("user.dir") + "/src/main/resources/AutomationSuite/Report_" + snewFilename1
+				+ ".html";
+		File file = new File(filePath);
+		// ExtentCucumberFormatter.setExtentHtmlReport(file);
+
+	}
 }
